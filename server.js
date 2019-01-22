50ea96bdc71dd1cd070cdf62b1c662a3d60b3e7d
BierinckxNicolas <nicolas.bierinckx@student.ap.be>
2019-01-22 15:31:22 +0100

    added view and start of methodes
    added add view and start of list view

--- server.js
+++ server.js
@@ -5,10 +5,46 @@ const MongoClient = require('mongodb').MongoClient
 
 var db;
 
-MongoClient.connect('mongodb://localhost:27017/inhaalexamens', (err, database) => {
+MongoClient.connect('mongodb://localhost:27017/examen', (err, database) => {
   if (err) return console.log(err)
-  db = database.db('inhaalexamens')
+  db = database.db('examen')
   app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000')
   })
 })
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.redirect('/list')
+ })
+ 
+ // List all products
+ app.get('/list', (req, res) => {
+   db.collection('products').find().toArray((err, result) => {
+     if (err) return console.log(err)
+     res.render('list.ejs', { products: result })
+   })
+ })
+ 
+ // Show the add product form
+ app.get('/add', (req, res) => {
+    res.render('add.ejs', {})
+ })
+ 
+ // Add a product to the db
+ app.post('/add', (req, res) => {
+   db.collection('inhaal').insertOne(req.body, (err, result) => {
+     if (err) return console.log(err)
+      res.redirect('/list')
+   })
+ })
+
+
+
+
+var d = new Date();
